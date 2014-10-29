762e4541ac0e1da3923f11d7bb87adcc12399a97
dehora <bill@dehora.net>
2014-10-29 09:02:19 +0000

    start 0.2.0

--- intercom-java/src/main/java/io/intercom/api/Intercom.java
+++ intercom-java/src/main/java/io/intercom/api/Intercom.java
@@ -14,7 +14,7 @@ public class Intercom {
 
     static final String AUTH_BEARER = "Bearer";
 
-    private static final String VERSION = "0.1.0";
+    private static final String VERSION = "0.2.0-SNAPSHOT";
 
     public static final String USER_AGENT = "intercom-java/" + Intercom.VERSION;
 
