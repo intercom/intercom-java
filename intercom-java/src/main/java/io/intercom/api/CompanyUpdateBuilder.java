4c474887bbbbeb72404646584f13f8dce88aee39
Prvoslav Savic <savic.prvoslav@gmail.com>
2016-01-22 15:08:00 +0100

    remove user from company fix

--- intercom-java/src/main/java/io/intercom/api/CompanyUpdateBuilder.java
+++ intercom-java/src/main/java/io/intercom/api/CompanyUpdateBuilder.java
@@ -1,46 +1,68 @@
 package io.intercom.api;
 
+import java.util.Iterator;
+import java.util.List;
 
 import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class CompanyUpdateBuilder {
 
-    /**
-     * Provide restrictions on the company data that can be sent via a user update
-     */
-    static List<CompanyWithStringPlan> buildUserUpdateCompanies(CompanyCollection add, CompanyCollection remove) {
+	/**
+	 * Provide restrictions on the company data that can be sent via a user
+	 * update
+	 */
+	static List<CompanyWithStringPlan> buildUserUpdateCompanies(CompanyCollection add, CompanyCollection remove) {
 
-        final ArrayList<CompanyWithStringPlan> updatableCompanies = Lists.newArrayList();
-        if (add != null) {
-            final List<Company> companies = add.getPage();
-            for (Company company : companies) {
-                updatableCompanies.add(prepareUpdatableCompany(company));
-            }
-        }
+		final List<CompanyWithStringPlan> updatableCompanies = Lists.newArrayList();
+		if (add != null) {
+			final List<Company> companies = add.getPage();
+			for (Company company : companies) {
+				updatableCompanies.add(prepareUpdatableCompany(company));
+			}
+		}
 
-        if (remove != null) {
-            final List<Company> companies = remove.getPage();
-            for (Company company : companies) {
-                updatableCompanies.add(prepareUpdatableCompany(company).setRemove(Boolean.TRUE));
-            }
-        }
+		if (remove != null) {
+			final List<Company> companies = remove.getPage();
 
-        return updatableCompanies;
-    }
+			for (Company company : companies) {
+				removeCompanyFromAddedList(updatableCompanies, company);
+				updatableCompanies.add(prepareUpdatableCompany(company).setRemove(Boolean.TRUE));
+			}
+		}
 
-    private static CompanyWithStringPlan prepareUpdatableCompany(Company company) {
-        final CompanyWithStringPlan updatableCompany = new CompanyWithStringPlan();
-        updatableCompany.setCompanyID(company.getCompanyID());
-        updatableCompany.setName(company.getName());
-        updatableCompany.setSessionCount(company.getSessionCount());
-        updatableCompany.setMonthlySpend(company.getMonthlySpend());
-        updatableCompany.setRemoteCreatedAt(company.getRemoteCreatedAt());
-        if (company.getPlan() != null) {
-            updatableCompany.setPlan(company.getPlan().getName());
-        }
-        return updatableCompany;
-    }
+		return updatableCompanies;
+	}
+
+	private static void removeCompanyFromAddedList(List<CompanyWithStringPlan> companies, Company company) {
+		Iterator<CompanyWithStringPlan> companyIterator = companies.iterator();
+
+		while (companyIterator.hasNext()) {
+			CompanyWithStringPlan companyWithStringPlan = companyIterator.next();
+			// TODO add equals and toHash methods to CompanyWithStringPlan and
+			// use it here
+			if (companyWithStringPlan.getId() != null && company.getId() != null && companyWithStringPlan.getId().equals(company.getId())) {
+				companyIterator.remove();
+				break;
+			}
+
+			if (companyWithStringPlan.getCompanyID() != null && company.getCompanyID() != null && companyWithStringPlan.getCompanyID().equals(company.getCompanyID())) {
+				companyIterator.remove();
+				break;
+			}
+		}
+	}
+
+	private static CompanyWithStringPlan prepareUpdatableCompany(Company company) {
+		final CompanyWithStringPlan updatableCompany = new CompanyWithStringPlan();
+		updatableCompany.setId(company.getId());
+		updatableCompany.setCompanyID(company.getCompanyID());
+		updatableCompany.setName(company.getName());
+		updatableCompany.setSessionCount(company.getSessionCount());
+		updatableCompany.setMonthlySpend(company.getMonthlySpend());
+		updatableCompany.setRemoteCreatedAt(company.getRemoteCreatedAt());
+		if (company.getPlan() != null) {
+			updatableCompany.setPlan(company.getPlan().getName());
+		}
+		return updatableCompany;
+	}
 }
