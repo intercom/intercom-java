066d8d4b9b361e90100a8d4d2e770a0face85198
Cathal Horan <cathal.horan@intercom.io>
2017-10-13 16:00:52 +0100

    Bumping version (error with previous bump version tag) (#153)

--- intercom-java/src/main/java/io/intercom/api/Intercom.java
+++ intercom-java/src/main/java/io/intercom/api/Intercom.java
@@ -15,7 +15,7 @@ public class Intercom {
         TOKEN
     }
 
-    private static final String VERSION = "2.2.8";
+    private static final String VERSION = "2.2.9";
 
     public static final String USER_AGENT = "intercom-java/" + Intercom.VERSION;
 
