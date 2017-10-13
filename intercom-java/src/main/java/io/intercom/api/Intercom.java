a0a4f9761bc79f1f052eb60f4601a1d4c745aa4e
Cathal Horan <cathal.horan@intercom.io>
2017-10-13 16:18:28 +0100

    Another issue with bintry version and tag release

--- intercom-java/src/main/java/io/intercom/api/Intercom.java
+++ intercom-java/src/main/java/io/intercom/api/Intercom.java
@@ -15,7 +15,7 @@ public class Intercom {
         TOKEN
     }
 
-    private static final String VERSION = "2.2.9";
+    private static final String VERSION = "2.2.10";
 
     public static final String USER_AGENT = "intercom-java/" + Intercom.VERSION;
 
