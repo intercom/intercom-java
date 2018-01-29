2d33c5040c2fe4310727a24b100e32d24e46ac47
Cathal Horan <cathal.horan@intercom.io>
2018-01-29 13:54:39 +0000

    Creating new release 2.3.1

--- intercom-java/src/main/java/io/intercom/api/Intercom.java
+++ intercom-java/src/main/java/io/intercom/api/Intercom.java
@@ -15,7 +15,7 @@ public class Intercom {
         TOKEN
     }
 
-    private static final String VERSION = "2.2.12";
+    private static final String VERSION = "2.3.1";
 
     public static final String USER_AGENT = "intercom-java/" + Intercom.VERSION;
 
