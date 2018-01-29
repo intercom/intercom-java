a4891c09c74354baad0867c9a89ecc914bff17ec
Timothy Lim <tim@thewheatfield.org>
2018-01-29 21:44:10 +0800

    Add support for more message attributes (#177)

--- intercom-java/src/main/java/io/intercom/api/ConversationMessage.java
+++ intercom-java/src/main/java/io/intercom/api/ConversationMessage.java
@@ -3,6 +3,8 @@ package io.intercom.api;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 @SuppressWarnings("UnusedDeclaration")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ConversationMessage extends TypedData {
@@ -10,6 +12,9 @@ public class ConversationMessage extends TypedData {
     @SuppressWarnings("FieldCanBeLocal")
     @JsonProperty("type")
     private final String type = "conversation_message";
+
+    @JsonProperty
+    private String id;
 
     @JsonProperty
     private String subject;
@@ -20,11 +25,21 @@ public class ConversationMessage extends TypedData {
     @JsonProperty
     private Author author;
 
+    @JsonProperty
+    private String url;
+
+    @JsonProperty("attachments")
+    private List<Attachment> attachments;
+
     public ConversationMessage() {
     }
 
     public String getType() {
         return type;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getSubject() {
@@ -39,11 +54,23 @@ public class ConversationMessage extends TypedData {
         return author;
     }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public List<Attachment> getAttachments() {
+        return attachments;
+    }
+
     @Override
     public int hashCode() {
         int result = subject != null ? subject.hashCode() : 0;
         result = 31 * result + (body != null ? body.hashCode() : 0);
         result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        result = 31 * result + (attachments != null ? attachments.hashCode() : 0);
+
         return result;
     }
 
@@ -58,6 +85,9 @@ public class ConversationMessage extends TypedData {
         if (body != null ? !body.equals(that.body) : that.body != null) return false;
         //noinspection RedundantIfStatement
         if (subject != null ? !subject.equals(that.subject) : that.subject != null) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (url != null ? !url.equals(that.url) : that.url != null) return false;
+        if (attachments != null ? !attachments.equals(that.attachments) : that.attachments != null) return false;
 
         return true;
     }
@@ -66,9 +96,12 @@ public class ConversationMessage extends TypedData {
     public String toString() {
         return "ConversationMessage{" +
             "type='" + type + '\'' +
+            ", id='" + id + '\'' +
             ", subject='" + subject + '\'' +
             ", body='" + body + '\'' +
             ", author=" + author +
+            ", url=" + url +
+            ", attachments=" + attachments +
             "} " + super.toString();
     }
 }
